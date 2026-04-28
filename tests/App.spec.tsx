4d9f15e3ab490d90b54e0c875e668c48f448902a
frankm24 <murphyfr@udel.edu>
2026-04-28 15:56:59 -0400

    Test movie editing: changes can be saved on save and don't apply when cancel pressed

--- tests/App.spec.tsx
+++ tests/App.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import App from "../src/App";
 import userEvent from "@testing-library/user-event";
 
@@ -46,4 +46,78 @@ describe("App Component", () => {
         expect(screen.queryByRole("button", {name: "Not liked"})).not.toBeInTheDocument();
         expect(screen.queryByRole("button", {name: "Liked"})).not.toBeInTheDocument();
     });
+    test("Movie can be edited and changes saved", () => {
+        const newTitle = "My Super Epic Movie";
+        const newReleaseYear = "1984";
+        const newRating = "10";
+        const newDescription = "This movie made billions for Frank Murphy and with those billions he made more movies."
+        const newSongName = "Joe's Blues";
+        const newSongBy = "Joe Pass";
+
+        render(<App />);
+        // Initially, editor pane is not visible.
+        expect(screen.queryAllByRole("button", {name: "Cancel"})).toHaveLength(0);
+        // Click edit on a movie
+        userEvent.click(screen.getAllByRole("button", {name: "Edit"})[0]);
+        // Movie editor should now be visible
+        expect(screen.getByRole("button", {name: "Cancel"})).toBeInTheDocument();
+        
+        // Change title 
+        const titleInput = screen.getByLabelText("Title:");
+        userEvent.clear(titleInput)
+        userEvent.type(titleInput, newTitle)
+        // Change release year
+        const yearInput = screen.getByRole("spinbutton");
+        userEvent.clear(yearInput);
+        userEvent.type(yearInput, newReleaseYear);
+        // Change rating
+        userEvent.selectOptions(screen.getByRole("combobox"), newRating);
+        // Change description
+        const descriptionInput = screen.getByLabelText("Description:");
+        userEvent.clear(descriptionInput);
+        userEvent.type(descriptionInput, newDescription)
+
+        // Get song items
+        const songItems = screen.getAllByRole("listitem");
+        const firstSongInputs = within(songItems[0]).getAllByRole("textbox")
+        // Change song name and ensure visible
+        // (cant test spoitify embed bc it does't actually change)
+        const songNameInput = firstSongInputs[0];
+        userEvent.clear(songNameInput);
+        userEvent.type(songNameInput, newSongName);
+        expect(songNameInput).toHaveValue(newSongName);
+        // Change song by and ensure change visible 
+        // (cant test spoitify embed bc it does't actually change)
+        const songByInput = firstSongInputs[1];
+        userEvent.clear(songByInput);
+        userEvent.type(songByInput, newSongBy);
+        expect(songByInput).toHaveValue(newSongBy);
+
+        // Click save
+        userEvent.click(screen.getByRole("button", {name: "Save"}));
+
+        // Verify that new properties saved 
+        expect(screen.getByText(newTitle)).toBeInTheDocument();
+        expect(screen.getByText(`Released ${newReleaseYear}`)).toBeInTheDocument();
+        expect(screen.getAllByText("⭐⭐⭐⭐⭐")).toHaveLength(3);
+        expect(screen.getByText(newDescription)).toBeInTheDocument();
+    });
+
+    test("Movie editing can be cancelled without saving", () => {
+        const newTitle = "Iron Man";
+
+        render(<App />);
+        // Click Edit on a movie
+        userEvent.click(screen.getAllByRole("button", {name: "Edit"})[0]);
+        // Change title of movie
+        const titleInput = screen.getByLabelText("Title:");
+        userEvent.clear(titleInput)
+        userEvent.type(titleInput, newTitle)
+        // Click cancel 
+        userEvent.click(screen.getByRole("button", {name: "Cancel"}))
+        // Ensure editor is closed and new title does not appear
+        expect(screen.queryByRole("button", {name: "Cancel"})).not.toBeInTheDocument();
+        expect(screen.queryByText(newTitle)).not.toBeInTheDocument();
+    })
+    
 });
